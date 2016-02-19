b2d4cce9e488dfc43d5e73e033ac7b43f6826735
Brian Gilham <me@briangilham.com>
2016-02-18 20:32:27 -0500

    Initial commit.

---
+++ Example/Pods/Target Support Files/TWGNotificationObserver/TWGNotificationObserver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TWGNotificationObserver : NSObject
+@end
+@implementation PodsDummy_TWGNotificationObserver
+@end
---
+++ Example/Pods/Target Support Files/TWGNotificationObserver/TWGNotificationObserver-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double TWGNotificationObserverVersionNumber;
+FOUNDATION_EXPORT const unsigned char TWGNotificationObserverVersionString[];
+
